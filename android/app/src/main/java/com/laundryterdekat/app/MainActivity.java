bf85d154beeb2398931a5d3d065a282b67156dd0
Yoswicaksono <yoswicaksono@gmail.com>
2026-01-29 15:12:53 +0700

    fix-middleware

---
+++ android/app/src/main/java/com/laundryterdekat/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.laundryterdekat.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -2,7 +2,7 @@ import { createServerClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
 export async function updateSession(request: NextRequest) {
-    letsupabaseResponse = NextResponse.next({
+    let supabaseResponse = NextResponse.next({
         request,
     })
 
